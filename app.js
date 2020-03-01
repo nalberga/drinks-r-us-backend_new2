a7e2813a3b79a11c39265bf8aa2ef94affe1d1c5
salientype <jorge@salientype.com>
2020-03-01 17:58:11 -0500

    finished API routes for Products

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.post('/api/users/register', function (req, res) {
     if (req.body.admin == null) {
         req.body.admin = false;
     }
-    
+
     const data = {
         f_name: req.body.f_name.trim(),
         l_name: req.body.l_name.trim(),
@@ -96,7 +96,7 @@ app.post('/api/users/register', function (req, res) {
         admin: req.body.admin,
     };
 
-    
+
     if (data.f_name && data.l_name && data.email && data.password) {
 
         var salt = bcrypt.genSaltSync(10);
@@ -118,7 +118,7 @@ app.post('/api/users/register', function (req, res) {
 
 // API update a target user's info
 app.put('/api/users/:id', function (req, res) {
-    
+
     const data = {
 
         id: req.params.id,
@@ -173,11 +173,11 @@ app.delete('/api/users/:id', function (req, res) {
     Users.findOne({ where: { id: id } }).then(results => {
 
         if (results) {
-            
+
             const targetUser = JSON.stringify(results);
 
             Users.destroy({ where: { id: id } }).then(function (user) {
-                
+
                 res.setHeader('Content-Type', 'application/json');
                 res.end(targetUser);
 
@@ -187,10 +187,10 @@ app.delete('/api/users/:id', function (req, res) {
 
             })
 
-        } 
+        }
 
     }).catch(function (e) {
-        
+
         console.log(e);
         res.status(434).send('error retrieving info on target User');
 
@@ -211,7 +211,49 @@ app.get('/api/products/', function (req, res) {
 
 });
 
-// API get target user
+// API get all products by type
+app.get('/api/products/type/:id', function (req, res) {
+
+    const id = req.params.id;
+
+    Products.findAll({ where: { type_id: id } }).then((results) => {
+
+        if (results.length != 0) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(results));
+        } else {
+            res.status(434).send('No products found belonging to this type!');
+        }
+
+    }).catch(function (e) {
+        console.log(e);
+        res.status(434).send('Error retrieving Products of this type');
+    })
+
+});
+
+// API get all products by category
+app.get('/api/products/category/:id', function (req, res) {
+
+    const id = req.params.id;
+
+    Products.findAll({ where: { category_id: id } }).then((results) => {
+
+        if (results.length != 0) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(results));
+        } else {
+            res.status(434).send('No products found belonging to this category!');
+        }
+
+    }).catch(function (e) {
+        console.log(e);
+        res.status(434).send('Error retrieving Products of this category');
+    })
+
+});
+
+// API get target product
 app.get('/api/products/:id', function (req, res) {
 
     let id = req.params.id;
@@ -222,12 +264,122 @@ app.get('/api/products/:id', function (req, res) {
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify(results));
         } else {
-            res.status(434).send('Product does not exist is DB');
+            res.status(434).send('Product not found');
         }
 
     }).catch(function (e) {
         console.log(e);
         res.status(434).send('error retrieving info on target Product');
+    })
+
+});
+
+// API add product
+app.post('/api/products/register', function (req, res) {
+
+    const data = {
+
+        name: req.body.name,
+        type_id: req.body.type_id,
+        category_id: req.body.category_id,
+        description: req.body.description,
+        quantity: req.body.quantity,
+        price: req.body.price,
+        image_url: req.body.image_url
+
+    };
+
+
+    if (data.name && data.type_id && data.category_id && data.description && data.quantity && data.price && data.image_url) {
+
+        Products.create(data).then(product => {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(product));
+        }).catch((e) => {
+            console.log(e);
+            res.status(434).send('error registering Product');
+        })
+
+    } else {
+
+        res.status(434).send('All fields are required to register.')
+
+    }
+
+});
+
+// API update a target product's info
+app.put('/api/products/:id', function (req, res) {
+
+    const data = {
+
+        id: req.params.id,
+        name: req.body.name,
+        type_id: req.body.type_id,
+        category_id: req.body.category_id,
+        description: req.body.description,
+        quantity: req.body.quantity,
+        price: req.body.price,
+        image_url: req.body.image_url
+
+    };
+
+    Products.findOne({ where: { id: data.id } }).then(product => {
+
+        product.update({
+
+            name: data.name,
+            type_id: data.type_id,
+            category_id: data.category_id,
+            description: data.description,
+            quantity: data.quantity,
+            price: data.price,
+            image_url: data.image_url,
+
+        }).then(newData => {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(newData));
+        }).catch((e) => {
+            res.status(434).send('unable to update Product')
+        })
+
+    }).catch((e) => {
+        console.log(e);
+        res.status(434).send(`unable to find Product ${data.id}`)
+
+    })
+
+});
+
+// API delete target Product
+app.delete('/api/products/:id', function (req, res) {
+
+    const id = req.params.id;
+
+    Products.findOne({ where: { id: id } }).then(results => {
+
+        if (results) {
+
+            const targetProduct = JSON.stringify(results);
+
+            Products.destroy({ where: { id: id } }).then(() => {
+
+                res.setHeader('Content-Type', 'application/json');
+                res.end(targetProduct);
+
+            }).catch((e) => {
+
+                res.status(434).send('unable to delete Product')
+
+            })
+
+        }
+
+    }).catch((e) => {
+
+        console.log(e);
+        res.status(434).send('error retrieving info on target Product');
+
     })
 
 });
